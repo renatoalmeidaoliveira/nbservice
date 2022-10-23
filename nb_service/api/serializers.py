35957671e59426522234d1eb0eeb28347b2e1e83
Jan Gilla <jan@jangilla.de>
2022-10-23 15:16:13 +0200

    Fixed import of missing django.conf.settings import.

--- nb_service/api/serializers.py
+++ nb_service/api/serializers.py
@@ -14,6 +14,7 @@ from drf_yasg.utils import swagger_serializer_method
 from rest_framework import serializers
 from django.contrib.contenttypes.models import ContentType
 
+from django.conf import settings
 from packaging import version
 NETBOX_CURRENT_VERSION = version.parse(settings.VERSION)
 if NETBOX_CURRENT_VERSION >= version.parse("3.3"):
