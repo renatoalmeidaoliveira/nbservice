fd9221330013c4ee6906c4f9434ccc87f4d20a6a
Renato Almeida de Oliveira <renato.almeida.oliveira@gmail.com>
2024-09-08 20:53:30 +0000

    Version 4.1.x support

--- nb_service/__init__.py
+++ nb_service/__init__.py
@@ -10,8 +10,8 @@ class NbserviceConfig(PluginConfig):
     version = __version__
     author = 'Renato Almeida de Oliveira Zaroubin'
     author_email = 'renato.almeida.oliveira@gmail.com'
-    min_version = "4.0.0"
-    max_version = "4.0.99"
+    min_version = "4.1.0"
+    max_version = "4.1.99"
     required_settings = []
     default_settings = {}
 
